ad79bab7a892c05ea940b504e08d3d991da28571
Jared Avila <jared.c.avila@gmail.com>
2019-05-22 18:56:00 -0700

    working on Burger logic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,23 @@ import classes from "./burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  let ingredientsArray = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      //stuff and things
+      return arr.concat(el);
+    }, []);
+  if (ingredientsArray.length === 0) {
+    ingredientsArray = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="produce" />
+      {ingredientsArray}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/components/container/BurgerBuilder/BurgerBuilder.js
+++ src/components/container/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component, Fragment } from "react";
 import Burger from "../../Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      bacon: 0,
+      cheese: 0,
+      meat: 2,
+      produce: 1
+    }
+  };
   render() {
     return (
       <Fragment>
-        <Burger></Burger>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Fragment>
     );
