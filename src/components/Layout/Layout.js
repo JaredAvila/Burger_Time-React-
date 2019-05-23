633d8a8cea535f630c89063d6c431d5247cc54c6
Jared Avila <jared.c.avila@gmail.com>
2019-05-22 17:36:39 -0700

    setting up initial structural components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
+import classes from "./layout.css";
 
 const Layout = props => (
   <Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.content}>{props.children}</main>
   </Fragment>
 );
 
