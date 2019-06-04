eb9c9ce94d15cd55c929375b4edd3357e331e2f2
Jared Avila <jared.c.avila@gmail.com>
2019-06-03 19:53:31 -0700

    added auth actions

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.css";
+import * as actions from "../../store/actions/";
 
 class Auth extends Component {
   state = {
@@ -82,6 +84,14 @@ class Auth extends Component {
     this.setState({ controls: updatedControls });
   };
 
+  submitHandler = event => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    );
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -93,7 +103,7 @@ class Auth extends Component {
 
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {formElementsArray.map(formElement => (
             <Input
               key={formElement.id}
@@ -113,4 +123,13 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const matDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
+
+export default connect(
+  null,
+  matDispatchToProps
+)(Auth);
